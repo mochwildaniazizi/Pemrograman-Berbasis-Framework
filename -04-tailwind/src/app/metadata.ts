d8453d6c0a17948cdd8b73674d360faf165a052e
wilo <wwilldanazizi@gmail.com>
2025-04-21 16:39:57 +0700

    finish-task2-js4

---
+++ -04-tailwind/src/app/metadata.ts
@@ -0,0 +1,10 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Tentang Saya",
+  description: "Halaman Tentang saya.",
+  openGraph: {
+    title: "Tentang Saya",
+    description: "Halaman Tentang Saya",
+  },
+};
